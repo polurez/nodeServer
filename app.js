5d8c39596e3c843ec654d9e8fb5a9b28771c8521
Igor <polurez@rambler.ru>
2017-07-05 18:49:56 +0300

    Save message from general (@testbot) channel and bot privat channel

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var slackbot = require('slackbots');
 var mongoClient = require("mongodb").MongoClient;
 
+
 var bot_token = require('./config').bot.bot_token;
 var bot_name = require('./config').bot.bot_name;
 var url = require('./config').url;
@@ -10,13 +11,41 @@ var bot = new slackbot({
     token: bot_token,
     name: bot_name
 });
+
 // event message
 bot.on('message', function (data) {
 
-    if ((data.type == "desktop_notification") && (data.channel == "D64FK5YQ7")) {
-        var messageForBot = {
-            content: data.content,
-            from: data.subtitle
+
+    /*if (data.type == "desktop_notification") {
+
+        if (data.subtitle == "#general") {
+
+            console.log(data.content.replace(/(.*): @(\w)+( )+/g, ""));
+            //console.log(data.subtitle);
+            
+            let name =data.content.match(/(\w).+(?=:)/i);
+            console.log(name[0]);
+        } else {
+
+            console.log(data.content);
+        }
+    }*/
+
+    if (data.type == "desktop_notification") {
+        if (data.subtitle == "#general") {
+
+            let content = data.content.replace(/(.*): @(\w)+( )+/g, "");
+            let name = data.content.match(/.*(?=:)/);
+            var messageForBot = {
+                content: content,
+                from: name[0]
+            }
+        } else {
+
+            var messageForBot = {
+                content: data.content,
+                from: data.subtitle
+            }
         }
 
         mongoClient.connect(url, function (err, db) {
